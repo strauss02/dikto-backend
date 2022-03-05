4e4391831622e60c845d421330941e4cb3a59176
Ido Strauss <strauss02@hotmail.co.il>
2022-03-05 21:37:21 +0200

    update cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const dotenv = require('dotenv')
 const wordRouter = require('./routers/wordRouter')
 const posRouter = require('./routers/posRouter')
 dotenv.config()
 
+app.use(cors())
 app.use('/word', wordRouter)
 app.use('/pos', posRouter)
 
